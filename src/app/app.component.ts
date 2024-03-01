715a75bef10c7af751a2dd64bbb7f944c9dabeb7
david-kolundzic <david-kolundzic@outlook.com>
2024-03-01 15:08:15 +0100

    test open new window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-azure-radiolist';
+
+  openRadioStation(radioAddress: string) {
+    window.open('http://radiostaticplayer.z6.web.core.windows.net', '_blank', 'location=yes,height=720,width=520,scrollbars=no,status=no');
+  }
 }
