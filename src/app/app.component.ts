e5b8d4f38a4f56274edc535cdfc187c194f0eb4e
david-kolundzic <david-kolundzic@outlook.com>
2024-03-18 15:52:42 +0100

    test open new window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'angular-azure-radiolist';
 
   openRadioStation(radioAddress: string) {
     window.open('http://radiostaticplayer.z6.web.core.windows.net', '_blank', 'location=yes,height=720,width=520,scrollbars=no,status=no');
+  }
+  openRadio() {
+    window.open('http://tanjakolundzic-001-site1.etempurl.com/', '_blank', 'location=yes,height=720,width=520,scrollbars=no,status=no');
   }
 }
